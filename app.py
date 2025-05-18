b26d5c779c33687ae0ce08e3f1e51bd2012a8b5d
DJpierre007 <djacobspierre@gmail.com>
2025-05-18 14:38:59 +0100

    app.py

--- app.py
+++ app.py
@@ -13,22 +13,14 @@ def search():
     data = request.json
     print("Received:", data)
 
-    return jsonify([
-        {
-            "product_name": "Brown Leather Boots",
-            "price": "£129",
-            "size_available": "10",
-            "image_url": "https://m.media-amazon.com/images/I/81ZDyj1EmmL._AC_UY500_.jpg",
-            "product_url": "https://www.amazon.co.uk/dp/B08H5P9L2L"
-        },
-        {
-            "product_name": "White Adidas Trainers",
-            "price": "£95",
-            "size_available": "9",
-            "image_url": "https://m.media-amazon.com/images/I/61qOT2LQfKL._AC_UX695_.jpg",
-            "product_url": "https://www.adidas.co.uk/product/white-trainers"
-        }
-    ])
+    return jsonify({
+        "product_name": "Brown Leather Boots",
+        "price": "£129",
+        "size_available": "10",
+        "image_url": "https://m.media-amazon.com/images/I/81ZDyj1EmmL._AC_UY500_.jpg",
+        "product_url": "https://www.amazon.co.uk/dp/B08H5P9L2L"
+    })
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
